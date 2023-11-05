eb5269e235c96d37bccb209892d36df490292d08
Aldin <62793769+aldinnasrunm@users.noreply.github.com>
2023-11-05 12:52:15 +0000

    some changes

--- jus.py
+++ jus.py
@@ -5,9 +5,12 @@ import os
 with open('juzData.json', 'r', encoding='utf-8') as juz_data_file:
     juz_data = json.load(juz_data_file)
 
+output_folder = 'juz'
+final_data_json = []
+
 # Create a folder for each juz_id
 for juz in juz_data['juzs']:
-    juz_id = juz['id']
+    juz_id = juz['id']#
     verse_mapping = juz['verse_mapping']
     # output_folder = f'juz_{juz_id}'
     # os.makedirs(output_folder, exist_ok=True)
@@ -16,11 +19,44 @@ for juz in juz_data['juzs']:
     # print(verse_mapping)
 
     for nosurah, ayat in verse_mapping.items():
-        ayatStart, ayatEnd = ayat.split('-')
-        # print(jus)
-        print("Ayat start : ",ayatStart)
-        print("Ayat end : ",ayatEnd)
-        with open('surah/'+nosurah+'.json', 'r', encoding='utf-8') as surah_file:
-            surah_file = json.load(surah_file)
         
-        print(surah_file['nama_latin'])
+        if juz_id > 0 :
+            ayatStart, ayatEnd = ayat.split('-')
+            # print(jus)
+            print("Ayat start : ",ayatStart)
+            print("Ayat end : ",ayatEnd)
+            with open('surah/'+nosurah+'.json', 'r', encoding='utf-8') as surah_file:
+                surah_file = json.load(surah_file)
+            
+            surah_data_json = {
+                    "status": surah_file["status"],
+        "nomor": surah_file["nomor"],
+        "nama": surah_file["nama"],
+        "nama_latin": surah_file["nama_latin"],
+        "jumlah_ayat": surah_file["jumlah_ayat"],
+        "tempat_turun": surah_file["tempat_turun"],
+        "arti": surah_file["arti"],
+        "deskripsi": surah_file["deskripsi"],
+        "audio": surah_file["audio"],
+        "ayat": surah_file["ayat"][int(ayatStart)-1:int(ayatEnd)]
+            }
+            # print(surah_file['nama_latin'])
+
+            print(surah_data_json)
+            final_data_json.append(surah_data_json)
+        
+#     final_data_json = {
+# "data": final_data_json
+# }
+    # with open('final_data.json', 'w', encoding='utf-8') as final_data_file:
+    #     json.dump(final_data_json, final_data_file, ensure_ascii=False, indent=4)
+
+    # # save into json file
+    # with open(f'{output_folder}', 'w', encoding='utf-8') as juz_file:
+    #     json.dump(final_data_json, juz_file, ensure_ascii=False, indent=4)
+# print(final_data_json)
+    juz_item_data_filename = os.path.join(output_folder, 'juz_item_data.json')
+    with open(juz_item_data_filename, 'w', encoding='utf-8') as juz_item_data_file:
+            json.dump(juz_item_data, juz_item_data_file, ensure_ascii=False, indent=2)
+
+        # break
